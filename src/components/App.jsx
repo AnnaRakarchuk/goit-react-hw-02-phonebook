7b30bf9a5b8e375024be281d39c520f554e77815
Anna Rakarchuk <anytarakarchuk@gmail.com>
2022-10-30 22:16:18 +0200

    Add part Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import {Form} from './Form/Form';
+
+export class App extends React.Component{
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  render(){
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <Form />
+      </div>);
+  }
+
+}
+
